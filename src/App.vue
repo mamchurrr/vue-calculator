23d295b7d17ac00c3c572b390e6f5d06d45608d4
mamchurrr <s.mamchurrr@gmail.com>
2020-01-19 17:36:42 +0200

    create calculate component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <Calculate
+            ref="calculate"
+            sum="withLogic"
+            subtraction
+            @logicSum="logicSum"
+            title="Welcome to Your Calculate Vue.js App"
+        />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculate from './components/Calculate.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'app',
+    components: {
+        Calculate
+    },
+    data() {
+        return {
+            result: 0
+        };
+    },
+    methods: {
+        logicSum(argA, argB) {
+            this.$refs.calculate.result = argA + argB + 1;
+            this.result = this.$refs.calculate.result;
+            return this.result;
+        }
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/Calculate.vue
@@ -0,0 +1,142 @@
+<template>
+    <div class="hello">
+        <h1>{{ title }}</h1>
+        <input type="number" v-model.number="argA" />
+        <input type="number" v-model.number="argB" />
+
+        <button v-if="sum" @click="calcSum">
+            сумма
+        </button>
+        <button v-if="subtraction" @click="calcSubtraction">
+            вычитание
+        </button>
+        <button v-if="multiplication" @click="calcMultiplication">
+            умножение
+        </button>
+        <button v-if="division" @click="calcDivision">
+            деление
+        </button>
+
+        <p>Результат: {{ result }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Calculate',
+    props: {
+        title: String,
+        sum: {
+            type: [Boolean, String],
+            default: true,
+            validator: function(value) {
+                if (typeof value !== 'boolean') {
+                    // The value must match one of these strings
+                    return ['withLogic'].indexOf(value) !== -1;
+                }
+                return true;
+            }
+        },
+        subtraction: {
+            type: [Boolean, String],
+            default: false,
+            validator: function(value) {
+                if (typeof value !== 'boolean') {
+                    // The value must match one of these strings
+                    return ['withLogic'].indexOf(value) !== -1;
+                }
+                return true;
+            }
+        },
+        multiplication: {
+            type: [Boolean, String],
+            default: false,
+            validator: function(value) {
+                if (typeof value !== 'boolean') {
+                    // The value must match one of these strings
+                    return ['withLogic'].indexOf(value) !== -1;
+                }
+                return true;
+            }
+        },
+        division: {
+            type: [Boolean, String],
+            default: false,
+            validator: function(value) {
+                if (typeof value !== 'boolean') {
+                    // The value must match one of these strings
+                    return ['withLogic'].indexOf(value) !== -1;
+                }
+                return true;
+            }
+        }
+    },
+    data() {
+        return {
+            result: 0,
+            argA: 0,
+            argB: 0
+        };
+    },
+    methods: {
+        calcSum() {
+            switch (this.sum) {
+                case 'withLogic':
+                    this.$emit('logicSum', this.argA, this.argB);
+                    break;
+                default:
+                    this.result = this.argA + this.argB;
+                    break;
+            }
+        },
+        calcSubtraction() {
+            switch (this.subtraction) {
+                case 'withLogic':
+                    this.$emit('logicSubtraction', this.argA, this.argB);
+                    break;
+                default:
+                    this.result = this.argA - this.argB;
+                    break;
+            }
+        },
+        calcMultiplication() {
+            switch (this.multiplication) {
+                case 'withLogic':
+                    this.$emit('logicMultiplication', this.argA, this.argB);
+                    break;
+                default:
+                    this.result = this.argA * this.argB;
+                    break;
+            }
+        },
+        calcDivision() {
+            switch (this.division) {
+                case 'withLogic':
+                    this.$emit('logicDivision', this.argA, this.argB);
+                    break;
+                default:
+                    this.result = this.argA / this.argB;
+                    break;
+            }
+        }
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+</style>
